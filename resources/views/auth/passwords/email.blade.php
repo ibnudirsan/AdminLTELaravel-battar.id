8066dd1b4a763675c92c9ccf11d571ac94e5d67d
ibnudirsan <ibnudirsan@gmail.com>
2020-05-30 01:30:38 +0700

    Login Form v2 by Colorlib

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -3,7 +3,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ __('System Login | Madinah Soft') }}</title>
+  <title>{{ __('Madinah Tech | Reset Password') }}</title>
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="{{ asset('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/bower_components/font-awesome/css/font-awesome.min.css') }}">
@@ -11,66 +11,130 @@
   <link rel="stylesheet" href="{{ asset('assets/dist/css/AdminLTE.min.css') }}">
   <link rel="stylesheet" href="{{ asset('assets/plugins/iCheck/square/blue.css') }}">
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+
+    <!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('login-assets/images/icons/favicon.ico') }}"/>
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/animate/animate.css') }}">
+    <!--===============================================================================================-->	
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/css-hamburgers/hamburgers.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/animsition/css/animsition.min.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/select2/select2.min.css') }}">
+    <!--===============================================================================================-->	
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/daterangepicker/daterangepicker.css') }}">
+    <!--===============================================================================================-->
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/css/util.css') }}">
+        <link rel="stylesheet" type="text/css" href="{{ asset('login-assets/css/main.css') }}">
+    <!--===============================================================================================-->
+
   <script src='https://www.google.com/recaptcha/api.js'></script>
   {!! NoCaptcha::renderJs() !!}
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-  <div class="login-box-body">
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+					<span class="login100-form-title p-b-10">
+						Madinah Tech
+					</span>
 
-    <div class="card-body">
-        @if (session('status'))
-            <div class="alert alert-success" role="alert">
-                {{ session('status') }}
-            </div>
-        @endif
-
-        <form method="POST" action="{{ route('password.email') }}">
-            @csrf
-
-            <div class="form-group row">
-                <label for="email" class="col-md-6 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                <div class="col-md-10">
-                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
-
-                    @error('email')
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $message }}</strong>
-                        </span>
-                    @enderror
-                </div>
-            </div>
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
 
-        {{-- <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                {!! app('captcha')->display() !!}
-            </div>
-        </div> --}}
+                    <form method="POST" action="{{ route('password.email') }}">
+                        @csrf
+            
+                        <div class="form-group row">
+                            <div class="col-md-10">
 
-            <div class="form-group row mb-0">
-                <div class="col-md-6 offset-md-4">
+                                <div class="wrap-input100 validate-input" data-validate = "Valid email is: a@b.c">
+                                    <input id="email" class="input100" type="text" name="email">
+                                    <span class="focus-input100" data-placeholder="Email"></span>
+                                </div>
+
+                                @error('email')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+            
+            
+                    <div class="form-group row mb-0">
+                        <div class="col-md-8 offset-md-4">
+                            {!! app('captcha')->display() !!}
+                        </div>
+                    </div>
+            
+                        <div class="form-group row mb-0">
+                            <div class="col-md-12 offset-md-4">            
+                                <div class="container-login100-form-btn">
+                                    <div class="wrap-login100-form-btn">
+                                        <div class="login100-form-bgbtn"></div>
+                                        <button class="login100-form-btn">
+                                            <i class="fa fa-repeat" aria-hidden="true"></i>
+                                            &nbsp;
+                                            Reset
+                                        </button>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    </form>
 
                     <a href="{{ url('/') }}">
-                        <button type="button" class="btn btn-danger btn-sm">
-                        <i class="fa fa-arrow-circle-left" aria-hidden="true"></i>
-                        {{ __('Back') }}
-                        </button>
+                        <div class="container-login100-form-btn">
+                            <div class="wrap-login100-form-btn">
+                                <div class="login100-form-bgbtn"></div>
+                                <button class="login100-form-btn">
+                                    <i class="fa fa-arrow-circle-left" aria-hidden="true"></i>
+                                    &nbsp;
+                                    Back
+                                </button>
+                            </div>
+                        </div>
                     </a>
 
-                    <button type="submit" class="btn btn-sm btn-primary">
-                        <i class="fa fa-repeat" aria-hidden="true"></i>
-                        {{ __('Reset') }}
-                    </button>
-                </div>
             </div>
-        </form>
+        </div>
+    </div>
 
-  </div>
-</div>
 <script src="{{ asset('assets/bower_components/jquery/dist/jquery.min.js') }}"></script>
 <script src="{{ asset('assets/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
 <script src="{{ asset('assets/plugins/iCheck/icheck.min.js') }}"></script>
+
+<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('login-assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/daterangepicker/moment.min.js') }}"></script>
+	<script src="{{ asset('login-assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/js/main.js') }}"></script>
 </body>
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,101 +1,108 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>{{ __('System Login | Madinah Soft') }}</title>
-  <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-  <link rel="stylesheet" href="{{ asset('assets/bower_components/bootstrap/dist/css/bootstrap.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/bower_components/font-awesome/css/font-awesome.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/bower_components/Ionicons/css/ionicons.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/dist/css/AdminLTE.min.css') }}">
-  <link rel="stylesheet" href="{{ asset('assets/plugins/iCheck/square/blue.css') }}">
-  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-  <script src='https://www.google.com/recaptcha/api.js'></script>
-  {!! NoCaptcha::renderJs() !!}
+	<title>Madinah Soft Tech | Login System</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+<!--===============================================================================================-->	
+	<link rel="icon" type="image/png" href="{{ asset('login-assets/images/icons/favicon.ico') }}"/>
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/bootstrap/css/bootstrap.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/fonts/iconic/css/material-design-iconic-font.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/animate/animate.css') }}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/css-hamburgers/hamburgers.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/animsition/css/animsition.min.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/select2/select2.min.css') }}">
+<!--===============================================================================================-->	
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/vendor/daterangepicker/daterangepicker.css') }}">
+<!--===============================================================================================-->
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/css/util.css') }}">
+	<link rel="stylesheet" type="text/css" href="{{ asset('login-assets/css/main.css') }}">
+<!--===============================================================================================-->
+<script src='https://www.google.com/recaptcha/api.js'></script>
+{!! NoCaptcha::renderJs() !!}
 </head>
-<body class="hold-transition login-page">
-<div class="login-box">
-    <div class="login-box-body">
-    <p class="login-box-msg">{{ __('Madinah Soft | Admin LTE') }}</p>
+<body>
+	
+	<div class="limiter">
+		<div class="container-login100">
+			<div class="wrap-login100">
+					<span class="login100-form-title p-b-10">
+						Madinah Tech
+					</span>
+					<span class="login100-form-title p-b-10">
+						<i class="zmdi zmdi-home"></i>
+					</span>
 
-    <form method="POST" action="{{ route('login') }}">
-        @csrf
+                    <form class="login100-form validate-form" method="POST" action="{{ route('login') }}">
+                        @csrf
 
-        <div class="form-group row">
-            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Username') }}</label>
-
-            <div class="col-md-8">
-                <input id="name" type="text" class="form-control @error('name') is-invalid @enderror" name="name" value="{{ old('name') }}" required autocomplete="name" autofocus>
-
-                @error('name')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-            <div class="col-md-8">
-                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
-
-                @error('password')
-                    <span class="invalid-feedback" role="alert">
-                        <strong>{{ $message }}</strong>
-                    </span>
-                @enderror
-            </div>
-        </div>
-
-        <div class="form-group row">
-            <div class="col-md-6 offset-md-4">
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                        <div class="wrap-input100 validate-input">
+                            <input id="name" class="input100" type="text" name="name">
+                            <span class="focus-input100" data-placeholder="Username"></span>
+                        </div>
     
-                    <label class="form-check-label" for="remember">
-                        {{ __('Remember Me') }}
-                    </label>
-                </div>
-            </div>
-        </div>
+                        <div class="wrap-input100 validate-input" data-validate="Enter password">
+                            <span class="btn-show-pass">
+                                <i class="zmdi zmdi-eye"></i>
+                            </span>
+                            <input id="password" class="input100" type="password" name="password">
+                            <span class="focus-input100" data-placeholder="Password"></span>
+                        </div>
+                        
+                        <div>
+                            {!! app('captcha')->display() !!}
+                        </div>
+    
+                        <div class="container-login100-form-btn">
+                            <div class="wrap-login100-form-btn">
+                                <div class="login100-form-bgbtn"></div>
+                                <button class="login100-form-btn">
+                                    Login
+                                </button>
+                            </div>
+                        </div>
+                    </form>
 
-        {{-- <div class="form-group row mb-0">
-            <div class="col-md-8 offset-md-4">
-                {!! app('captcha')->display() !!}
-            </div>
-        </div> --}}
+					<div class="text-center p-t-10">
+						<span class="txt1">
+							Lupa Password?
+						</span>
 
-    <div class="form-group row mb-0">
-        <div class="col-md-8 offset-md-4">
-            <button type="submit" class="btn btn-primary">
-                {{ __('Login') }}
-            </button>
-        </div>
-    </div>
+						<a class="txt2" href="{{ route('password.request') }}">
+							Reset Password
+						</a>
+					</div>
+			</div>
+		</div>
+	</div>
+	
 
-    </form>
+	<div id="dropDownSelect1"></div>
+	
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/jquery/jquery-3.2.1.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/animsition/js/animsition.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/bootstrap/js/popper.js') }}"></script>
+	<script src="{{ asset('login-assets/vendor/bootstrap/js/bootstrap.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/select2/select2.min.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/daterangepicker/moment.min.js') }}"></script>
+	<script src="{{ asset('login-assets/vendor/daterangepicker/daterangepicker.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/vendor/countdowntime/countdowntime.js') }}"></script>
+<!--===============================================================================================-->
+	<script src="{{ asset('login-assets/js/main.js') }}"></script>
 
-
-    <p class="mb-1">
-        <a href="{{ route('password.request') }}">{{ __('Lupa Password') }}</a>
-    </p>
-
-    </div>
-</div>
-<script src="{{ asset('assets/bower_components/jquery/dist/jquery.min.js') }}"></script>
-<script src="{{ asset('assets/bower_components/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-<script src="{{ asset('assets/plugins/iCheck/icheck.min.js') }}"></script>
-<script>
-  $(function () {
-    $('input').iCheck({
-      checkboxClass: 'icheckbox_square-blue',
-      radioClass: 'iradio_square-blue',
-      increaseArea: '20%'
-    });
-  });
-</script>
 </body>
 </html>
